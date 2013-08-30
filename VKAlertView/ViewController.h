f661b80c1d16d4a7a9fbe9e7e9c193629235cc95
Vladimir Kolbas <vkolbas@mac.com>
2013-08-30 23:41:58 +0200

    Initial Commit

---
+++ VKAlertView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VKAlertView
+//
+//  Created by Vladimir Kolbas on 8/30/13.
+//  Copyright (c) 2013 Vladimir Kolbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VKAlertView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VKAlertView
+//
+//  Created by Vladimir Kolbas on 8/30/13.
+//  Copyright (c) 2013 Vladimir Kolbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
